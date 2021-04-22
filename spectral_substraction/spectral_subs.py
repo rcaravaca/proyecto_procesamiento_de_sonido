623a5555c1713ab844156fc881620923c59f7d43
cebrenes07 <cebrenes07@gmail.com>
2021-04-21 23:32:21 -0600

    add another code implementation of spectral subs and add subspace approach

---
+++ spectral_substraction/spectral_subs.py
@@ -0,0 +1,14 @@
+import pyroomacoustics as pra
+
+# import or create `noisy_signal`
+denoised_signal = pra.apply_spectral_sub(noisy_signal, nfft=512,
+                                     db_reduc=10, lookback=5,
+                                     beta=20, alpha=3)
+'''
+Parameters:	
+.- nfft (int) – FFT size. Length of gain filter, i.e. the number of frequency bins, is given by nfft//2+1.
+.- db_reduc (float) – Maximum reduction in dB for each bin.
+.- lookback (int) – How many frames to look back for the noise estimate.
+.- beta (float) – Overestimation factor to “push” the gain filter value (at each frequency) closer to the dB reduction specified by db_reduc.
+.- alpha (float, optional) – Exponent factor to modify transition behavior towards the dB reduction specified by db_reduc. Default is 1.
+'''
