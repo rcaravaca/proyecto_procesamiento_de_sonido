2d70dd90c27a2d3b337f19551742315b3a0826fe
rcaravaca <ronycaravaca0691@gmail.com>
2021-04-24 14:03:33 -0600

    Update

--- run_filter.sh
+++ run_filter.sh
@@ -3,8 +3,10 @@ if [[ $# -eq 0 ]] ; then
 	echo 'SH_ERROR: No arguments'
 	exit 1
 else
+
 	mkdir $1/original
 	mkdir $1/filtered
+
 	for file in $(ls $1/*lin*); do
 		file_name=`ls $file | sed 's/.wav//g'` 
 		aux_name=`ls $file | sed 's/.wav/_.wav/g'`
@@ -22,7 +24,8 @@ else
 		echo ""
 	done
 
-	echo "SH_INFO: Moving to each dir by SNR!"
+	echo "SH_INFO: Moving to each dir by SNR!" 
+
 	mkdir $1/original/snr_-10
 	mkdir $1/original/snr_-5
 	mkdir $1/original/snr_0
@@ -34,17 +37,43 @@ else
 	mkdir $1/filtered/snr_5
 	mkdir $1/filtered/snr_10
 
-	mv $1/original/*pasos_10minutossnr_-10_mix_*wav $1/original/snr_-10
-	mv $1/original/*pasos_10minutossnr_-5_mix_*wav $1/original/snr_-5
-	mv $1/original/*pasos_10minutossnr_0_mix_*wav $1/original/snr_0
-	mv $1/original/*pasos_10minutossnr_5_mix_*wav $1/original/snr_5
-	mv $1/original/*pasos_10minutossnr_10_mix_*wav $1/original/snr_10
+	mkdir $1/original/snr_-10/carolina $1/original/snr_-10/joseline
+	mkdir $1/original/snr_-5/carolina $1/original/snr_-5/joseline
+	mkdir $1/original/snr_0/carolina $1/original/snr_0/joseline
+	mkdir $1/original/snr_5/carolina $1/original/snr_5/joseline
+	mkdir $1/original/snr_10/carolina $1/original/snr_10/joseline
+	
+	mkdir $1/filtered/snr_-10/carolina $1/filtered/snr_-10/joseline
+	mkdir $1/filtered/snr_-5/carolina $1/filtered/snr_-5/joseline
+	mkdir $1/filtered/snr_0/carolina $1/filtered/snr_0/joseline
+	mkdir $1/filtered/snr_5/carolina $1/filtered/snr_5/joseline
+	mkdir $1/filtered/snr_10/carolina $1/filtered/snr_10/joseline
 
-	mv $1/filtered/*pasos_10minutossnr_-10*wav $1/filtered/snr_-10
-	mv $1/filtered/*pasos_10minutossnr_-5*wav $1/filtered/snr_-5
-	mv $1/filtered/*pasos_10minutossnr_0_mix_*wav $1/filtered/snr_0
-	mv $1/filtered/*pasos_10minutossnr_5_mix_*wav $1/filtered/snr_5
-	mv $1/filtered/*pasos_10minutossnr_10_mix_*wav $1/filtered/snr_10
+	#### Carolina
+	mv $1/original/*carolina*pasos_10minutossnr_-10_mix_*wav $1/original/snr_-10/carolina
+	mv $1/original/*carolina*pasos_10minutossnr_-5_mix_*wav $1/original/snr_-5/carolina
+	mv $1/original/*carolina*pasos_10minutossnr_0_mix_*wav $1/original/snr_0/carolina
+	mv $1/original/*carolina*pasos_10minutossnr_5_mix_*wav $1/original/snr_5/carolina
+	mv $1/original/*carolina*pasos_10minutossnr_10_mix_*wav $1/original/snr_10/carolina
+
+	mv $1/filtered/*carolina*pasos_10minutossnr_-10*wav $1/filtered/snr_-10/carolina
+	mv $1/filtered/*carolina*pasos_10minutossnr_-5*wav $1/filtered/snr_-5/carolina
+	mv $1/filtered/*carolina*pasos_10minutossnr_0_mix_*wav $1/filtered/snr_0/carolina
+	mv $1/filtered/*carolina*pasos_10minutossnr_5_mix_*wav $1/filtered/snr_5/carolina
+	mv $1/filtered/*carolina*pasos_10minutossnr_10_mix_*wav $1/filtered/snr_10/carolina
+
+	#### Joseline
+	mv $1/original/*joseline*pasos_10minutossnr_-10_mix_*wav $1/original/snr_-10/joseline
+	mv $1/original/*joseline*pasos_10minutossnr_-5_mix_*wav $1/original/snr_-5/joseline
+	mv $1/original/*joseline*pasos_10minutossnr_0_mix_*wav $1/original/snr_0/joseline
+	mv $1/original/*joseline*pasos_10minutossnr_5_mix_*wav $1/original/snr_5/joseline
+	mv $1/original/*joseline*pasos_10minutossnr_10_mix_*wav $1/original/snr_10/joseline
+
+	mv $1/filtered/*joseline*pasos_10minutossnr_-10*wav $1/filtered/snr_-10/joseline
+	mv $1/filtered/*joseline*pasos_10minutossnr_-5*wav $1/filtered/snr_-5/joseline
+	mv $1/filtered/*joseline*pasos_10minutossnr_0_mix_*wav $1/filtered/snr_0/joseline
+	mv $1/filtered/*joseline*pasos_10minutossnr_5_mix_*wav $1/filtered/snr_5/joseline
+	mv $1/filtered/*joseline*pasos_10minutossnr_10_mix_*wav $1/filtered/snr_10/joseline
 
 
 fi
