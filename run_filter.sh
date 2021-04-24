0b889133f10fbc4cdf5c2e69042d851569ffb82b
rcaravaca <ronycaravaca0691@gmail.com>
2021-04-24 14:52:12 -0600

    Update run_filter.sh

--- run_filter.sh
+++ run_filter.sh
@@ -42,7 +42,7 @@ else
 	mkdir $1/original/snr_0/carolina $1/original/snr_0/joseline
 	mkdir $1/original/snr_5/carolina $1/original/snr_5/joseline
 	mkdir $1/original/snr_10/carolina $1/original/snr_10/joseline
-	
+
 	mkdir $1/filtered/snr_-10/carolina $1/filtered/snr_-10/joseline
 	mkdir $1/filtered/snr_-5/carolina $1/filtered/snr_-5/joseline
 	mkdir $1/filtered/snr_0/carolina $1/filtered/snr_0/joseline
@@ -56,8 +56,8 @@ else
 	mv $1/original/*carolina*pasos_10minutossnr_5_mix_*wav $1/original/snr_5/carolina
 	mv $1/original/*carolina*pasos_10minutossnr_10_mix_*wav $1/original/snr_10/carolina
 
-	mv $1/filtered/*carolina*pasos_10minutossnr_-10*wav $1/filtered/snr_-10/carolina
-	mv $1/filtered/*carolina*pasos_10minutossnr_-5*wav $1/filtered/snr_-5/carolina
+	mv $1/filtered/*carolina*pasos_10minutossnr_-10_mix_*wav $1/filtered/snr_-10/carolina
+	mv $1/filtered/*carolina*pasos_10minutossnr_-5_mix_*wav $1/filtered/snr_-5/carolina
 	mv $1/filtered/*carolina*pasos_10minutossnr_0_mix_*wav $1/filtered/snr_0/carolina
 	mv $1/filtered/*carolina*pasos_10minutossnr_5_mix_*wav $1/filtered/snr_5/carolina
 	mv $1/filtered/*carolina*pasos_10minutossnr_10_mix_*wav $1/filtered/snr_10/carolina
@@ -69,11 +69,85 @@ else
 	mv $1/original/*joseline*pasos_10minutossnr_5_mix_*wav $1/original/snr_5/joseline
 	mv $1/original/*joseline*pasos_10minutossnr_10_mix_*wav $1/original/snr_10/joseline
 
-	mv $1/filtered/*joseline*pasos_10minutossnr_-10*wav $1/filtered/snr_-10/joseline
-	mv $1/filtered/*joseline*pasos_10minutossnr_-5*wav $1/filtered/snr_-5/joseline
+	mv $1/filtered/*joseline*pasos_10minutossnr_-10_mix_*wav $1/filtered/snr_-10/joseline
+	mv $1/filtered/*joseline*pasos_10minutossnr_-5_mix_*wav $1/filtered/snr_-5/joseline
 	mv $1/filtered/*joseline*pasos_10minutossnr_0_mix_*wav $1/filtered/snr_0/joseline
 	mv $1/filtered/*joseline*pasos_10minutossnr_5_mix_*wav $1/filtered/snr_5/joseline
 	mv $1/filtered/*joseline*pasos_10minutossnr_10_mix_*wav $1/filtered/snr_10/joseline
 
+	### Create train/test original
+	echo "SH_INFO: Split by train and test!" 
+	mkdir $1/original/snr_-10/carolina/train $1/original/snr_-10/joseline/train
+	mkdir $1/original/snr_-5/carolina/train $1/original/snr_-5/joseline/train
+	mkdir $1/original/snr_0/carolina/train $1/original/snr_0/joseline/train
+	mkdir $1/original/snr_5/carolina/train $1/original/snr_5/joseline/train
+	mkdir $1/original/snr_10/carolina/train $1/original/snr_10/joseline/train
+	
+	mkdir $1/filtered/snr_-10/carolina/train $1/filtered/snr_-10/joseline/train
+	mkdir $1/filtered/snr_-5/carolina/train $1/filtered/snr_-5/joseline/train
+	mkdir $1/filtered/snr_0/carolina/train $1/filtered/snr_0/joseline/train
+	mkdir $1/filtered/snr_5/carolina/train $1/filtered/snr_5/joseline/train
+	mkdir $1/filtered/snr_10/carolina/train $1/filtered/snr_10/joseline/train
+
+	### Create train/test filtered
+	mkdir $1/original/snr_-10/carolina/test $1/original/snr_-10/joseline/test
+	mkdir $1/original/snr_-5/carolina/test $1/original/snr_-5/joseline/test
+	mkdir $1/original/snr_0/carolina/test $1/original/snr_0/joseline/test
+	mkdir $1/original/snr_5/carolina/test $1/original/snr_5/joseline/test
+	mkdir $1/original/snr_10/carolina/test $1/original/snr_10/joseline/test
+	
+	mkdir $1/filtered/snr_-10/carolina/test $1/filtered/snr_-10/joseline/test
+	mkdir $1/filtered/snr_-5/carolina/test $1/filtered/snr_-5/joseline/test
+	mkdir $1/filtered/snr_0/carolina/test $1/filtered/snr_0/joseline/test
+	mkdir $1/filtered/snr_5/carolina/test $1/filtered/snr_5/joseline/test
+	mkdir $1/filtered/snr_10/carolina/test $1/filtered/snr_10/joseline/test
+
+	### Moving carloina original
+	mv $1/original/snr_-10/carolina/*_mix_1??.wav $1/original/snr_-10/carolina/test/
+	mv $1/original/snr_-10/carolina/*_mix_???.wav $1/original/snr_-10/carolina/train/
+	mv $1/original/snr_-5/carolina/*_mix_1??.wav $1/original/snr_-5/carolina/test/
+	mv $1/original/snr_-5/carolina/*_mix_???.wav $1/original/snr_-5/carolina/train/
+	mv $1/original/snr_0/carolina/*_mix_1??.wav $1/original/snr_0/carolina/test/
+	mv $1/original/snr_0/carolina/*_mix_???.wav $1/original/snr_0/carolina/train/
+	mv $1/original/snr_5/carolina/*_mix_1??.wav $1/original/snr_5/carolina/test/
+	mv $1/original/snr_5/carolina/*_mix_???.wav $1/original/snr_5/carolina/train/
+	mv $1/original/snr_10/carolina/*_mix_1??.wav $1/original/snr_10/carolina/test/
+	mv $1/original/snr_10/carolina/*_mix_???.wav $1/original/snr_10/carolina/train/
+
+	### Moving joseline original
+	mv $1/original/snr_-10/joseline/*_mix_1??.wav $1/original/snr_-10/joseline/test/
+	mv $1/original/snr_-10/joseline/*_mix_???.wav $1/original/snr_-10/joseline/train/
+	mv $1/original/snr_-5/joseline/*_mix_1??.wav $1/original/snr_-5/joseline/test/
+	mv $1/original/snr_-5/joseline/*_mix_???.wav $1/original/snr_-5/joseline/train/
+	mv $1/original/snr_0/joseline/*_mix_1??.wav $1/original/snr_0/joseline/test/
+	mv $1/original/snr_0/joseline/*_mix_???.wav $1/original/snr_0/joseline/train/
+	mv $1/original/snr_5/joseline/*_mix_1??.wav $1/original/snr_5/joseline/test/
+	mv $1/original/snr_5/joseline/*_mix_???.wav $1/original/snr_5/joseline/train/
+	mv $1/original/snr_10/joseline/*_mix_1??.wav $1/original/snr_10/joseline/test/
+	mv $1/original/snr_10/joseline/*_mix_???.wav $1/original/snr_10/joseline/train/
+
+	### Moving carloina filtered
+	mv $1/filtered/snr_-10/carolina/*_mix_filtered_1??.wav $1/filtered/snr_-10/carolina/test/
+	mv $1/filtered/snr_-10/carolina/*_mix_filtered_???.wav $1/filtered/snr_-10/carolina/train/
+	mv $1/filtered/snr_-5/carolina/*_mix_filtered_1??.wav $1/filtered/snr_-5/carolina/test/
+	mv $1/filtered/snr_-5/carolina/*_mix_filtered_???.wav $1/filtered/snr_-5/carolina/train/
+	mv $1/filtered/snr_0/carolina/*_mix_filtered_1??.wav $1/filtered/snr_0/carolina/test/
+	mv $1/filtered/snr_0/carolina/*_mix_filtered_???.wav $1/filtered/snr_0/carolina/train/
+	mv $1/filtered/snr_5/carolina/*_mix_filtered_1??.wav $1/filtered/snr_5/carolina/test/
+	mv $1/filtered/snr_5/carolina/*_mix_filtered_???.wav $1/filtered/snr_5/carolina/train/
+	mv $1/filtered/snr_10/carolina/*_mix_filtered_1??.wav $1/filtered/snr_10/carolina/test/
+	mv $1/filtered/snr_10/carolina/*_mix_filtered_???.wav $1/filtered/snr_10/carolina/train/
+
+	### Moving joseline filtered
+	mv $1/filtered/snr_-10/joseline/*_mix_filtered_1??.wav $1/filtered/snr_-10/joseline/test/
+	mv $1/filtered/snr_-10/joseline/*_mix_filtered_???.wav $1/filtered/snr_-10/joseline/train/
+	mv $1/filtered/snr_-5/joseline/*_mix_filtered_1??.wav $1/filtered/snr_-5/joseline/test/
+	mv $1/filtered/snr_-5/joseline/*_mix_filtered_???.wav $1/filtered/snr_-5/joseline/train/
+	mv $1/filtered/snr_0/joseline/*_mix_filtered_1??.wav $1/filtered/snr_0/joseline/test/
+	mv $1/filtered/snr_0/joseline/*_mix_filtered_???.wav $1/filtered/snr_0/joseline/train/
+	mv $1/filtered/snr_5/joseline/*_mix_filtered_1??.wav $1/filtered/snr_5/joseline/test/
+	mv $1/filtered/snr_5/joseline/*_mix_filtered_???.wav $1/filtered/snr_5/joseline/train/
+	mv $1/filtered/snr_10/joseline/*_mix_filtered_1??.wav $1/filtered/snr_10/joseline/test/
+	mv $1/filtered/snr_10/joseline/*_mix_filtered_???.wav $1/filtered/snr_10/joseline/train/
 
 fi
