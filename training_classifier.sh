60821ec3d78d7676d5638b2f275cf9d19821509e
rcaravaca <ronycaravaca0691@gmail.com>
2021-07-14 21:12:36 -0600

    adding some changes

--- training_classifier.sh
+++ training_classifier.sh
@@ -10,6 +10,7 @@ for audio_type in $AUDIO_TYPE; do
 		for filter in $FILTER; do 
 			for snr in $SNR; do 
 				for clss in $CLASS; do 
+					mkdir -p trained_models/${noise}/${audio_type}/${filter}/${snr}/ && touch trained_models/${noise}/${audio_type}/${filter}/${snr}/foo.file
 					echo "python3 pyAudioAnalysis/pyAudioAnalysis/audioAnalysis.py trainClassifier -i dataset/pasos_${noise}_carolina_joseline_michelle/${filter}/${audio_type}/${snr}/carolina/train/carolina/ dataset/pasos_${noise}_carolina_joseline_michelle/${filter}/${audio_type}/${snr}/joseline/train/joseline/ --method $clss -o trained_models/${noise}/${audio_type}/${filter}/${snr}/model" >> ${noise}_training_exec.sh
 				done
 			done
