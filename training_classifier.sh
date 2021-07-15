52152f71c1a65c397929da07aa6f819f08f0dc16
rcaravaca <ronycaravaca0691@gmail.com>
2021-07-14 21:42:15 -0600

    adding files

--- training_classifier.sh
+++ training_classifier.sh
@@ -2,17 +2,25 @@ NOISE="oficina babble ruido_blanco"
 FILTER="mmse spect_sub wiener"
 SNR="snr_0 snr_-10 snr_10 snr_-5 snr_5"
 CLASS="svm knn randomforest"
-AUDIO_TYPE="filtered original"
+audio_type="original"
 
 rm *_training_exec.sh
-for audio_type in $AUDIO_TYPE; do
-	for noise in $NOISE; do
+for noise in $NOISE; do
+	for snr in $SNR; do 
+		for clss in $CLASS; do 
+			mkdir -p trained_models/${noise}/${audio_type}/${filter}/${snr}/ && touch trained_models/${noise}/${audio_type}/${snr}/foo.file
+			echo "python3 pyAudioAnalysis/pyAudioAnalysis/audioAnalysis.py trainClassifier -i dataset/pasos_${noise}_carolina_joseline_michelle/${audio_type}/${snr}/carolina/train/carolina/ dataset/pasos_${noise}_carolina_joseline_michelle/${audio_type}/${snr}/joseline/train/joseline/ --method $clss -o trained_models/${noise}/${audio_type}/${filter}/${snr}/model" >> ${noise}_training_exec.sh
+		done
+	done
+done
+
+audio_type="filtered"
+for noise in $NOISE; do
+	for snr in $SNR; do 
 		for filter in $FILTER; do 
-			for snr in $SNR; do 
-				for clss in $CLASS; do 
-					mkdir -p trained_models/${noise}/${audio_type}/${filter}/${snr}/ && touch trained_models/${noise}/${audio_type}/${filter}/${snr}/foo.file
-					echo "python3 pyAudioAnalysis/pyAudioAnalysis/audioAnalysis.py trainClassifier -i dataset/pasos_${noise}_carolina_joseline_michelle/${filter}/${audio_type}/${snr}/carolina/train/carolina/ dataset/pasos_${noise}_carolina_joseline_michelle/${filter}/${audio_type}/${snr}/joseline/train/joseline/ --method $clss -o trained_models/${noise}/${audio_type}/${filter}/${snr}/model" >> ${noise}_training_exec.sh
-				done
+			for clss in $CLASS; do 
+				mkdir -p trained_models/${noise}/${audio_type}/${filter}/${snr}/ && touch trained_models/${noise}/${audio_type}/${filter}/${snr}/foo.file
+				echo "python3 pyAudioAnalysis/pyAudioAnalysis/audioAnalysis.py trainClassifier -i dataset/pasos_${noise}_carolina_joseline_michelle/${filter}/${audio_type}/${snr}/carolina/train/carolina/ dataset/pasos_${noise}_carolina_joseline_michelle/${filter}/${audio_type}/${snr}/joseline/train/joseline/ --method $clss -o trained_models/${noise}/${audio_type}/${filter}/${snr}/model" >> ${noise}_training_exec.sh
 			done
 		done
 	done
